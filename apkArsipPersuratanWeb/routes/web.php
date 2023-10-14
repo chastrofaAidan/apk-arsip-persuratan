252002e953363920494a64c7a3c5e8e44307740b
chastrofaAidan <aidandeandra@gmail.com>
2023-10-14 18:48:27 +0700

    BackEnd Start

--- apkArsipPersuratanWeb/routes/web.php
+++ apkArsipPersuratanWeb/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/walas','WalasController@index');
+Route::get('/walas/tambah','WalasController@tambah');
+Route::post('/walas/store','WalasController@store');
+Route::get('/walas/edit/{id}','WalasController@edit');
+Route::post('/walas/update','WalasController@update');
+Route::get('/walas/hapus/{id}','WalasController@hapus');
