f3183b6ff3f0392bcfb5ce9e3dd3ac788af33a9d
darvaaph <darva99801@gmail.com>
2023-10-15 01:15:06 +0700

    nambah login page dan sidebar

--- apkArsipPersuratanWeb/resources/views/aidan.blade.php
+++
@@ -1,1 +0,0 @@
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias eligendi quis tenetur, repudiandae quam exercitationem maiores. Adipisci distinctio fugiat libero ut deserunt neque consequuntur itaque ex. Facilis sit illum rem!
--- apkArsipPersuratanWeb/resources/views/login.blade.php
+++
@@ -1,1 +0,0 @@
-awkowkoakwdokawodkaowkdaowkdoawkdoaw konto
